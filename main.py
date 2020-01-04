0aa76e4de162c3be5160bcde8236cec775b39c71
TheTechJumbo <shaikhm-y15@mgs.org>
2020-01-04 19:22:59 +0000

    Task 2

--- main.py
+++ main.py
@@ -8,70 +8,80 @@ contracts=[]
 cases=[]
 chargers=[]
 totalPrice=0
-
-#choose a phone or tablet
-deviceType="a"
-while deviceType != "PHONE" and deviceType != "TABLET" :
-	deviceType=input("Enter 'PHONE' or 'TABLET' to choose which type of device you would like: ")
-print()
-if deviceType == "phone":
-	deviceRange=primaryKey[0:6]
-	print("These are your possible phone choices along with their item codes and prices: ")
+repeatPurchase="YES"
+while repeatPurchase=="YES":
+	#choose a phone or tablet
+	deviceType="a"
+	while deviceType != "PHONE" and deviceType != "TABLET" :
+		deviceType=input("Enter 'PHONE' or 'TABLET' to choose which type of device you would like: ")
 	print()
-	for phone in range(6):
-		print("•"+str(descriptions[phone])+" for £"+str(prices[phone])+". Item code: "+str(primaryKey[phone]))
+	if deviceType == "phone":
+		deviceRange=primaryKey[0:6]
+		print("These are your possible phone choices along with their item codes and prices: ")
 		print()
-else:
-	deviceRange=primaryKey[6:10]
-	print("These are your possible tablet choices along with their item codes and prices: ")
-	print()
-	for tablet in range(4):
-		tablet+=6
-		print("•"+str(descriptions[tablet])+" for £"+str(prices[tablet])+". Item code: "+str(primaryKey[tablet]))
+		for phone in range(6):
+			print("•"+str(descriptions[phone])+" for £"+str(prices[phone])+". Item code: "+str(primaryKey[phone]))
+			print()
+	else:
+		deviceRange=primaryKey[6:10]
+		print("These are your possible tablet choices along with their item codes and prices: ")
 		print()
-deviceDec="a"
-while deviceDec not in deviceRange:
+		for tablet in range(4):
+			tablet+=6
+			print("•"+str(descriptions[tablet])+" for £"+str(prices[tablet])+". Item code: "+str(primaryKey[tablet]))
+			print()
+	deviceDec="a"
+	while deviceDec not in deviceRange:
+		print()
+		deviceDec=input("From the above list, enter the item code for the device that you want to purchase: ")
+	deviceIdx=primaryKey.index(deviceDec)
+	devices.append(deviceIdx)
 	print()
-	deviceDec=input("From the above list, enter the item code for the device that you want to purchase: ")
-deviceIdx=primaryKey.index(deviceDec)
-devices.append(deviceIdx)
-print()
 
-#allow phone customers to choose whether the phone will be SIM Free or Pay As You Go
-contractDec="a"
-while contractDec not in primaryKey[-7:-5]:
+	#allow phone customers to choose whether the phone will be SIM Free or Pay As You Go
+	contractDec="a"
+	while contractDec not in primaryKey[-7:-5]:
+		print()
+		contractDec=input("Enter the item code for whichever contract you would like, either SIM Free(Item code: SMNO) or Pay As You Go(Item code: SMPG): ")
+	contractIdx=primaryKey.index(contractDec)
+	contracts.append(contractIdx)
+
+	#allow the customer to choose a standard or luxury case
+	caseDec="a"
+	while caseDec not in primaryKey[-5:-3]:
+		print()
+		caseDec=input("Enter the item code for whichever case you would like, either standard(Item code: CSST) or luxury case(Item code: CSLX)? ")
+	caseIdx=primaryKey.index(caseDec)
+	cases.append(caseIdx)
+
+	#allow the customer to choose the chargers required
+	chargerDec="a"
+	while chargerDec not in primaryKey[-3:]:
+		print()
+		chargerDec=input("Enter the item code for whichever charger you would like, either a car charger(Item code: CGCR), a home charger(Item code: CGHM) or 'both'. If you want both chargers, type 'BOTH'. ")
+	chargerIdx=primaryKey.index(chargerDec)
+	chargers.append(chargerIdx)
+
+	#output a list of the items purchased and print the total price of this transaction
 	print()
-	contractDec=input("Enter the item code for whichever contract you would like, either SIM Free(Item code: SMNO) or Pay As You Go(Item code: SMPG): ")
-contractIdx=primaryKey.index(contractDec)
-contracts.append(contractIdx)
-
-#allow the customer to choose a standard or luxury case
-caseDec="a"
-while caseDec not in primaryKey[-5:-3]:
+	print("Your basket:")
+	for item in range(len(devices)):
+		item-=1
+		print("•"+str(descriptions[devices[item]]))
+		print("•"+str(descriptions[contracts[item]]))
+		print("•"+str(descriptions[cases[item]]))
+		print("•"+str(descriptions[chargers[item]]))
+		currentPrice=prices[devices[item]]+prices[contracts[item]]+prices[cases[item]]+prices[chargers[item]]
+	totalPrice+=currentPrice
 	print()
-	caseDec=input("Enter the item code for whichever case you would like, either standard(Item code: CSST) or luxury case(Item code: CSLX)? ")
-caseIdx=primaryKey.index(caseDec)
-cases.append(caseIdx)
+	print("The total cost of your basket is £"+str(totalPrice))
 
-#allow the customer to choose the chargers required
-chargerDec="a"
-while chargerDec not in primaryKey[-3:]:
-	print()
-	chargerDec=input("Enter the item code for whichever charger you would like, either a car charger(Item code: CGCR), a home charger(Item code: CGHM) or 'both'. If you want both chargers, type 'BOTH'. ")
-chargerIdx=primaryKey.index(chargerDec)
-chargers.append(chargerIdx)
-
-#output a list of the items purchased and print the total price of this transaction
-print()
-print("Your basket:")
-for item in range(len(devices)):
-	item-=1
-	print("•"+str(descriptions[devices[item]]))
-	print("•"+str(descriptions[contracts[item]]))
-	print("•"+str(descriptions[cases[item]]))
-	print("•"+str(descriptions[chargers[item]]))
-	currentPrice=prices[devices[item]]+prices[contracts[item]]+prices[cases[item]]+prices[chargers[item]]
-totalPrice+=currentPrice
-print()
+#Task 2
+	repeatPurchase="a"
+	while repeatPurchase != "YES" and repeatPurchase != "NO":
+		repeatPurchase=input("Would you like to buy any additional devices? Answer with 'YES' or NO': ")
+		print()
+	if repeatPurchase == "NO":
+		break
 print("The total cost of your basket is £"+str(totalPrice))
 
